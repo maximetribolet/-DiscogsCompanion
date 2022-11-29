2d85a0c74b05a39b9789f3867d0419d7b56f1949
Raphael Ventura <raphaelventura55@gmail.com>
2022-11-29 15:36:12 +0100

    fixed nested routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_110619) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_141932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,13 +64,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_110619) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "discogs_username"
     t.string "first_name"
     t.string "last_name"
     t.string "country"
     t.string "address"
-    t.string "postcode"
+    t.string "plz"
     t.string "phone_number"
+    t.string "discogs_username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
