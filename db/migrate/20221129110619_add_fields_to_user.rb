9ac7a7c968b16197fd70a031204f84e7983e51a1
Maxime Tribolet <maxime.tribolet@gmail.com>
2022-11-29 14:52:26 +0100

    conflict resolved

--- db/migrate/20221129110619_add_fields_to_user.rb
+++ db/migrate/20221129110619_add_fields_to_user.rb
@@ -1,10 +1,11 @@
 class AddFieldsToUser < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :discogs_username, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :country, :string
     add_column :users, :address, :string
-    add_column :users, :plz, :string
+    add_column :users, :postcode, :string
     add_column :users, :phone_number, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_29_110619) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,16 +69,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_110619) do
     t.string "last_name"
     t.string "country"
     t.string "address"
-    t.string "plz"
+    t.string "postcode"
     t.string "phone_number"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "alerts", "products"
   add_foreign_key "alerts", "users"
-
   add_foreign_key "matches", "alerts"
   add_foreign_key "matches", "products"
   add_foreign_key "products", "users"
