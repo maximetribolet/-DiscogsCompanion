2f679d79bb390c7b17aabfb7ca6226c7fcb4af03
Max <mxhmmrstn@gmail.com>
2022-11-29 17:08:27 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    country: Faker::Address.country,
+    address: Faker::Address.street_address,
+    plz: Faker::Address.zip,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+  user.save!
+end
+puts 'Finished!'
+
+# puts 'Creating 20 fake products...'
+# 20.times do
+#   product = Product.new(
+#     media_format:,
+#     album_title:,
+#     artist:,
+#     release_date:,
+#     genre:,
+#     lowest_price:,
+#     median_price:,
+#     seller_id:,
+#     product_url:
+#   )
+#   product.save!
+# end
+# puts 'Finished!'
