0c27eceeb22e4207b17985a61dc5846e9e613c8d
Raphael Ventura <raphaelventura55@gmail.com>
2022-11-30 16:32:29 +0100

    controllers revised

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,18 @@
 class AlertsController < ApplicationController
+  def index
+    @alert = Alert.where(user: current_user)
+  end
+
   def create
     @alert = Alert.new(alert_params)
+    @alert.user = current_user
+    # assign this aletr to current user
+    # normal if save flow
+    if @alert.save
+      redirect_to products_path, notice: "alert creted"
+    else
+      render new:, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -16,6 +28,7 @@ class AlertsController < ApplicationController
   end
 
   def alert_params
-    params.require(:alert).permit(:min_media_condition, :min_sleeve_condition, :country, :max_price, :auto_buy, :alert_duration_days, :seller_rating)
+    params.require(:alert).permit(:min_media_condition, :min_sleeve_condition, :country, :max_price, :auto_buy,
+                                  :alert_duration_days, :seller_rating, :product_id)
   end
 end
